8451e9fd3c4381febec6df89ae48c3274b24fadd
Kuba Jezusek <jakub.jezusek@gmail.com>
2016-05-31 14:13:46 -0500

    Experiment Large

---
+++ scripts/eecs-349-experiment-large_new.sh
@@ -0,0 +1,11 @@
+python train.py \
+       --data_file=data/eecs349-data.txt \
+       --num_epochs=50 \
+       --hidden_size=256 \
+       --num_layers=1 \
+       --model="rnn" \
+       --batch_size=64 \
+       --output_dir=large \
+       --dropout = 0.1, 0.3, 0.5 \
+
+tensorboard --logdir=large/tensorboard_log/ --port=6007
